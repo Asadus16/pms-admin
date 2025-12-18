7e8c4aa0d7d1957756bd9d6c43ed6f0e2a244035
Asadus16 <asadslayer999@gmail.com>
2025-12-18 17:29:37 +0500

    new

---
+++ app/property-manager-signup/page.jsx
@@ -0,0 +1,7 @@
+'use client';
+
+import PropertyManagerSignup from '@pages/PropertyManagerSignup';
+
+export default function PropertyManagerSignupPage() {
+  return <PropertyManagerSignup />;
+}
--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom'
 import UserTypeSelection from './pages/UserTypeSelection'
 import Login from './pages/Login'
 import Dashboard from './pages/dashboard'
+import PropertyManagerSignup from './pages/PropertyManagerSignup'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<UserTypeSelection />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/property-manager-signup" element={<PropertyManagerSignup />} />
       <Route path="/dashboard/*" element={<Dashboard />} />
     </Routes>
   )
